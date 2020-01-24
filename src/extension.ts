f7f40a680a3f024d7739fd73af92c0cf3e37c307
niharika <niharika.sweta@broadcom.com>
2020-01-24 11:11:30 +0100

    pushing real-extension demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -13,14 +14,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let helloDisposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(helloDisposable);
+    let fileDisposable = vscode.commands.registerCommand('extension.insertLink',()=>{
+		vscode.window.showInformationMessage('welcome file disposable');
+
+	});
+
+	context.subscriptions.push(fileDisposable);
+
+	let figDisposable = vscode.commands.registerCommand('extension.insertFig',()=>{
+	
+		vscode.window.showInformationMessage('welcome figure disposable');
+
+	});
+context.subscriptions.push(figDisposable);
 }
 
 // this method is called when your extension is deactivated
